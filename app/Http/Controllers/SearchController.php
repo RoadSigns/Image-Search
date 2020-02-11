5e1ad871f4891cc1fdd9dbf25887fd2a2f8736b7
Zack <zack@zacklott.co.uk>
2020-02-11 23:01:53 +0000

    Create the Search Method in the Search Controller

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,14 +3,18 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Contracts\ImageRepositoryInterface;
 use App\Http\Requests\SearchRequest;
+use App\Models\ImageSearch;
 use Illuminate\View\View;
 
 class SearchController extends Controller
 {
-    public function __construct()
-    {
+    public ImageRepositoryInterface $imageRepository;
 
+    public function __construct(ImageRepositoryInterface $imageRepository)
+    {
+        $this->imageRepository = $imageRepository;
     }
 
     public function index(): View
@@ -18,8 +22,13 @@ class SearchController extends Controller
         return view('index');
     }
 
-    public function search(SearchRequest $searchRequest): View
+    public function search(SearchRequest $searchRequest)
     {
-        dd($searchRequest);
+        $imageSearch = ImageSearch::create($searchRequest->all());
+        $images = $this->imageRepository->search($imageSearch);
+
+        return ($images->total())
+        ? view('search', ['images' => $images->appends(request()->input())])
+        : redirect(route('index'))->with(['error' => 'Currently unable to display images']);
     }
 }
